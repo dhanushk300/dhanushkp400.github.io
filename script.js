8f39349117b3c37f35244f85bf91a7b2d6358ac2
dhanushk300 <dhanushkp72@gmail.com>
2025-09-14 20:58:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-$(window).on("scroll", function() {
-  // Animate skill bars
+// Fade-in sections on scroll
+$(window).on('scroll', function() {
+  $('section').each(function() {
+    if ($(window).scrollTop() + $(window).height() > $(this).offset().top + 100) {
+      $(this).addClass('show');
+    }
+  });
+
+  // Animate skill bars if Skills section exists
   $("#skills .skill-bar").each(function() {
     let barPos = $(this).offset().top;
     let windowPos = $(window).scrollTop() + $(window).height();
