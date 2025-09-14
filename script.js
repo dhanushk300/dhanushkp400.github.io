12e5f753c5d520cebca82da1389008556aaa37de
dhanushk300 <dhanushkp72@gmail.com>
2025-09-14 18:42:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-$(document).ready(function() {
-  // Smooth Scroll
-  $("nav a").on("click", function(e) {
-    if (this.hash !== "") {
-      e.preventDefault();
-      var hash = this.hash;
-      $("html, body").animate({
-        scrollTop: $(hash).offset().top - 70
-      }, 800);
+$(window).on("scroll", function() {
+  // Animate skill bars
+  $("#skills .skill-bar").each(function() {
+    let barPos = $(this).offset().top;
+    let windowPos = $(window).scrollTop() + $(window).height();
+    if (windowPos > barPos) {
+      $(this).css("width", $(this).data("skill"));
     }
   });
-
-  // Reveal sections on scroll
-  $(window).on("scroll", function() {
-    $("section").each(function() {
-      var top = $(this).offset().top - window.innerHeight + 100;
-      if ($(window).scrollTop() > top) {
-        $(this).addClass("show");
-      }
-    });
-  });
-
-  // Animate skill bars when skills page loads
-  if ($("#skills").length) {
-    $(".skill-bar").each(function() {
-      var width = $(this).data("skill");
-      $(this).animate({ width: width }, 2000);
-    });
-  }
 });
